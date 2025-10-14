8a355fb3bf937644eceaf7a1001274dc83677fe6
CassieProgram <shynar.largess@testpro.io>
2025-10-13 22:32:44 -0400

    Homework-21
    rename playlist

---
+++ src/test/java/Homework21renamePlaylist.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework21renamePlaylist extends BaseTest{
+   //Prerequisite - at least one user -created playlist
+    String newPlaylistName = " Sample Edited Playlist";
+
+    @Test
+
+    public void renamePlaylist(){
+
+        String updatePlaylistMsg = "Updated playlist\"Sample Edited Playlist.\"";
+
+        provideEmail("shynar@testpro.io");
+        providePassword("Javashynar890@");
+        clickSubmit();
+        doubleClickPlaylist();
+        enterNewPlaylistName();
+        Assert.assertEquals(getRenamePlaylistSuccessMsg(),updatePlaylistMsg);
+    }
+
+
+
+    public void doubleClickPlaylist(){
+        WebElement playlistElement = BaseTest.wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        actions.doubleClick(playlistElement).perform();
+
+    }
+public void enterNewPlaylistName() {
+        WebElement playlistInputField = BaseTest.wait.until (ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='name']")));
+        playlistInputField.sendKeys(Keys.chord(Keys.CONTROL,"A", Keys.BACK_SPACE));
+
+        playlistInputField.sendKeys(newPlaylistName);
+        playlistInputField.sendKeys(Keys.ENTER);
+
+}
+public String getRenamePlaylistSuccessMsg() {
+        WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
+        return notification.getText();
+}
+}
