a259fee2d1277be5027fe3c37d34e6a6d5d3eb3b
CassieProgram <shynar.largess@testpro.io>
2025-10-15 14:32:43 -0400

    Homework-22
    refactor

--- src/test/java/Homework22renamePlaylist.java
+++ src/test/java/Homework22renamePlaylist.java
@@ -3,38 +3,35 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
-@Ignore
-
-public class Homework21renamePlaylist extends BaseTest{
+public class Homework22renamePlaylist extends BaseTest {
    //Prerequisite - at least one user -created playlist
     String newPlaylistName = " Sample Edited Playlist";
-@Ignore
+
     @Test
 
-    public void renamePlaylist(){
+    public void renamePlaylist() throws InterruptedException{
 
-        String updatePlaylistMsg = "Updated playlist\"Sample Edited Playlist.\"";
+        String updatePlaylistMsg = "Updated playlist \"Sample Edited Playlist.\"";
 
         provideEmail("shynar@testpro.io");
         providePassword("Javashynar890@");
         clickSubmit();
         doubleClickPlaylist();
         enterNewPlaylistName();
-        Assert.assertEquals(getRenamePlaylistSuccessMsg(),updatePlaylistMsg);
+        Assert.assertEquals(getRenamePlaylistSuccessMsg(), updatePlaylistMsg);
     }
 
 
 
     public void doubleClickPlaylist(){
-        WebElement playlistElement = BaseTest.wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
         actions.doubleClick(playlistElement).perform();
 
     }
 public void enterNewPlaylistName() {
-        WebElement playlistInputField = BaseTest.wait.until (ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='name']")));
+        WebElement playlistInputField = wait.until (ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='name']")));
         playlistInputField.sendKeys(Keys.chord(Keys.CONTROL,"A", Keys.BACK_SPACE));
 
         playlistInputField.sendKeys(newPlaylistName);
